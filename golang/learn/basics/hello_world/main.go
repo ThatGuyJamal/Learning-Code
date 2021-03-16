ce380044691e7d4f36e933fa71547f6bb71dc02b
Jamal L <jamallyons8@gmail.com>
2021-03-16 11:21:16 -0400

    adding go lang code.

---
+++ golang/learn/basics/hello_world/main.go
@@ -0,0 +1,14 @@
+package main
+
+// importing the fmt package for logging to the console
+import (
+	"fmt"
+)
+
+//Starting a runtime function
+func main() {
+	// Printing "hello world!"
+	fmt.Println("Hello World!")
+}
+
+//? You can use "go build" to create a compiled version of the code.
