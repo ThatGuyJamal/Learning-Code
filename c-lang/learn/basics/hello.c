c05a450db9ac0eba350bd6317e81a00d062b06d8
ThatGuyJamal <jamallyons8@gmail.com>
2021-05-19 10:35:31 -0400

    Small changes

--- c-lang/learn/basics/hello.c
+++ c-lang/learn/basics/hello.c
@@ -1,17 +1,16 @@
 // ? import the stdio library (the name stands for standard input-output library).
 
-// This library gives us access to input/output functions.
-
 #include <stdio.h>
 
+// This library gives us access to input/output functions.
 // stdio is the library that provides the printf() function.
 
 // ? This function is wrapped into a main() function. The main() function is the entry point of any C program.
 
-
 // void means the function does not take any arguments.
 
-int main(void) {
+int main(void)
+{
     printf("Hello, World!");
     return 0;
 }
