f3b1c2a7690f4823e70f77269a2c8d12bdd52b13
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2021-10-16 22:29:28 -0400

    Adding Static golang server

---
+++ golang/samples/http_playground/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var PORT = 8080
+
+func main() {
+
+	// The first line of code creates the file server object using the FileServer function.
+	// This function accepts a path in the http.Dir type.
+	// Therefore, we have to convert the string path “./static” to an http.Dir path type.
+	fileServer := http.FileServer(http.Dir("./static"))
+	// This function acts in the same way as the HandleFunc function, with some small differences.
+	http.Handle("/", fileServer)
+	// handles the form data and functions
+	fmt.Printf("Starting server at port %v", PORT)
+
+	// The ListenAndServe method is exported by the http packet we imported during step one.
+	// This method allows us to startw the web server and specify the port to listen for incoming requests.
+	if server := http.ListenAndServe(":8080", nil); server != nil {
+		// if an error on server start, return in.
+		log.Fatal(server)
+	}
+
+}
+
+// go run server.go
+// http://localhost:8080/
